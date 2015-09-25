22627489d2df78c752193784fcfdeac142420ee8
Aleksi Savijoki <aleksi.savijoki@tawasta.fi>
2015-09-25 16:20:14 +0300

    Added web_graph_improved as a dependency for showing multiple measures in bar charts.

--- __openerp__.py
+++ __openerp__.py
@@ -7,7 +7,7 @@ Module for presenting reports of projectmodule.
     """,
     
     'author': 'Aleksi Savijoki',
-    'depends': ['project'],
+    'depends': ['project', 'web_graph_improved'],
     'version': '1.0',
     'data': [ 'project_project_view.xml', 'project_report_view.xml'],
     'installable': True,
