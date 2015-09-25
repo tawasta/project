39a60d0a3df73ed60218f71f809fbeb6d8e72f76
Aleksi Savijoki <aleksi.savijoki@tawasta.fi>
2015-09-25 16:19:26 +0300

    Changed the implementation: sql view is made in four functions (init, _group_by, _select, _from) and the class isn't inherited anymore.

--- project_report.py
+++ project_report.py
@@ -3,70 +3,61 @@
 from openerp import models, fields, tools
 
 class project_report(models.Model):
-	_name = "project.report"
-	_inherit = "report.project.task.user"
+	_name = "project.report" 
+	_description = "Reports about projects"
+	_auto = False
+ 
+	name = fields.Char('Name of project', readonly=True)
+	planned_hours = fields.Float('Planned hours', readonly=True)
+	effective_hours = fields.Float('Effective hours', readonly=True)
+	avg_price = fields.Float('Average price per hour',default=10.5)
+	estimated_cost = fields.Float('Estimated cost of project', readonly=True)
+	current_cost = fields.Float('Current cost of project', readonly=True)
 
 
-	avg_price = fields.Float("Average price",default=10.5)
+	def _select(self):
+		select_str = "SELECT "
+		select_str += "min(p.id) as id,"
+		select_str += "a.name as name,"
+		select_str += "p.planned_hours as planned_hours,"
+		select_str += "p.effective_hours as effective_hours,"
+		select_str += "avg_price,"
+		select_str += "(avg_price*planned_hours) as estimated_cost,"
+		select_str += "(avg_price*effective_hours) as current_cost"	
+		
+		return select_str
 
-	_order = 'name desc, project_id'
+	def _from(self):
 
+		from_str = "project_project p "
+		from_str += "JOIN account_analytic_account a "
+		from_str += "ON p.analytic_account_id = a.id"
+		
+		return from_str
+
+
+	def _group_by(self):
+
+		group_by_str = " GROUP BY "
+		group_by_str += "p.id,"
+		group_by_str += "a.name,"
+		group_by_str += "planned_hours,"
+		group_by_str += "effective_hours,"
+		group_by_str += "avg_price,"
+		group_by_str += "estimated_cost,"
+		group_by_str += "current_cost"
+		
+		return group_by_str
+
+				
 	def init(self, cr):
-		tools.sql.drop_view_if_exists(cr, 'report_project_task_user')
-		cr.execute("""
-			CREATE view report_project_task_user as
-			SELECT
-			(select 1 ) AS nbr,
-			t.id as id,
-			t.date_start as date_start,
-			t.date_end as date_end,
-			t.date_last_stage_update as date_last_stage_update,
-			t.date_deadline as date_deadline,
-			abs((extract('epoch' from (t.write_date-t.date_start)))/(3600*24))  as no_of_days,
-			t.user_id,
-			t.reviewer_id,
-			t.progress as progress,
-			t.project_id,
-			t.effective_hours as hours_effective,
-			t.priority,
-			t.name as name,
-			t.company_id,
-			t.partner_id,
-			t.stage_id as stage_id,
-			t.kanban_state as state,
-			t.remaining_hours as remaining_hours,
-			t.total_hours as total_hours,
-			delay_hours as hours_delay,
-			t.planned_hours as hours_planned,
-			(extract('epoch' from (t.write_date-t.create_date)))/(3600*24)  as closing_days,
-			(extract('epoch' from (t.date_start-t.create_date)))/(3600*24)  as opening_days,
-			(extract('epoch' from (t.date_deadline-(now() at time zone 'UTC'))))/(3600*24)  as delay_endings_days,
-			p.avg_price as avg_price
-			FROM project_task t 
-			JOIN project_project p
-			ON p.id = t.project_id
-			WHERE t.active = 'true'
-			GROUP BY
-			t.id,
-			remaining_hours,
-			t.effective_hours,
-			progress,
-			t.total_hours,
-			t.planned_hours,
-			hours_delay,
-			t.create_date,
-			t.write_date,
-			t.date_start,
-			t.date_end,
-			t.date_deadline,
-			t.date_last_stage_update,
-			t.user_id,
-			t.reviewer_id,
-			t.project_id,
-			t.priority,
-			name,
-			t.company_id,
-			t.partner_id,
-			stage_id,
-			avg_price
-			""")
+		self._table = 'project_report'
+		tools.sql.drop_view_if_exists(cr, self._table)
+		cr.execute(""" 
+			CREATE or REPLACE VIEW %s as (
+				%s
+				FROM %s
+				%s
+				)""" % (self._table, self._select(),
+                    	self._from(), self._group_by()))
+
