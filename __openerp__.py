7b64aa901a4e9dd38c2752a2fcd2bf5ec8dfc99d
Aleksi Savijoki <aleksi.savijoki@tawasta.fi>
2015-10-14 14:59:54 +0300

    Started a new project to implement project feed to form views. The purpose of this model is to give a fast breif what has been done in tasks from the newest changes towards oldest.

---
+++ __openerp__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Project feed',
+    'description': """
+
+        Module for project events feed.
+
+    """,
+    
+    'author': 'Aleksi Savijoki',
+    'depends': ['project'],
+    'version': '1.0',
+    'data': [
+        'views/project_feed_view.xml'
+    ],
+    'installable': True,
+    'auto-install': False,
+    
+}
---
+++ models/project_feed.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+from openerp import api, fields, models
+
+class ProjectFeed(models.Model):
+
+	_inherit = 'project.project'
+
+	project_tasks = fields.One2many(
+		'project.task',
+		'project_id',
+		string='All tasks',
+		compute='compute_task_lines'
+	)
+
+
+	task_work = fields.One2many(
+		'project.task.work',
+		'task_id',
+		string='Work lines',
+		compute='compute_task_lines'
+	)
+
+	@api.one
+	def compute_task_lines(self):
+
+		# Get the project_task_works and order them DESC by write_date
+		self.task_work = self.env['project.task.work'].search([('task_id','in', self.tasks.ids)], order='write_date DESC')
+
