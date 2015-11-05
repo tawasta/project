9501333516f2970b26f8712736166ca812c2db62
Aleksi Savijoki <aleksi.savijoki@tawasta.fi>
2015-11-05 15:48:32 +0200

    Modified modules structure according to template module

--- models/project_feed.py
+++ models/project_feed.py
@@ -1,11 +1,25 @@
 # -*- coding: utf-8 -*-
 
+# 1. Standard library imports:
+
+# 2. Known third party imports:
+
+# 3. Odoo imports (openerp):
 from openerp import api, fields, models
+
+# 4. Imports from Odoo modules:
+
+# 5. Local imports in the relative form:
+
+# 6. Unknown third party imports:
+
 
 class ProjectFeed(models.Model):
 
+	# 1. Private attributes
 	_inherit = 'project.project'
 
+	# 2. Fields declaration
 	task_work = fields.One2many(
 		'project.task.work',
 		'task_id',
@@ -13,6 +27,9 @@ class ProjectFeed(models.Model):
 		compute='compute_task_lines'
 	)
 
+	# 3. Default methods
+
+    # 4. Compute and search fields, in the same order that fields declaration
 	@api.one
 	def compute_task_lines(self):
 
@@ -20,4 +37,11 @@ class ProjectFeed(models.Model):
 		self.task_work = self.env['project.task.work'].search([('task_id',
 			'in', self.tasks.ids)], order='write_date DESC')
 
+	# 5. Constraints and onchanges
 
+    # 6. CRUD methods
+
+    # 7. Action methods
+
+    # 8. Business methods
+    
