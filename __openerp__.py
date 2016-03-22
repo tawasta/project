9afab838620e93db9c18463d425e5b42fadcf59a
Aleksi Savijoki <aleksi.savijoki@tawasta.fi>
2016-03-22 14:55:05 +0200

    Gathers tasks' messages and adds them to the feed. Feed is implemented using transitional model called project.event.line which combines messages and task works into feed. Duplicates are removed from the table.

--- __openerp__.py
+++ __openerp__.py
@@ -26,7 +26,7 @@
         Module for project events feed.
 
     """,
-    'version': '8.0.0.1.1',
+    'version': '8.0.0.1.2',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
     'license': 'AGPL-3',
--- models/project_feed.py
+++ models/project_feed.py
@@ -5,7 +5,7 @@
 # 2. Known third party imports:
 
 # 3. Odoo imports (openerp):
-from openerp import api, fields, models
+from openerp import api, fields, models, tools
 
 # 4. Imports from Odoo modules:
 
@@ -14,29 +14,56 @@ from openerp import api, fields, models
 # 6. Unknown third party imports:
 
 
-class ProjectFeed(models.TransientModel):
+class ProjectFeed(models.Model):
 
 	# 1. Private attributes
 	_inherit = 'project.project'
 
-	# 2. Fields declaration
-	task_work = fields.One2many(
-		'account.analytic.line',
-		'task_id',
-		string='Work lines',
-		compute='compute_task_lines'
+
+
+	event_lines = fields.One2many(
+		'project.event.line',
+		'project_id',
+		string='Event lines',
+		compute='compute_event_lines'
 	)
 
 	# 3. Default methods
 
-    # 4. Compute and search fields, in the same order that fields declaration
-	@api.one
-	def compute_task_lines(self):
+	# 4. Compute and search fields, in the same order that fields declaration
+	@api.multi
+	def compute_event_lines(self):
+
+		self._cr.execute('truncate table project_event_line')
 
 		# Get the project_task_works and order them DESC by write_date
-		self.task_work = self.env['account.analytic.line'].search([('task_id',
+		task_works = self.env['account.analytic.line'].search([('task_id',
 			'in', self.tasks.ids)], order='write_date DESC')
 
+		# Get tasks messages and order them DESC by write_date
+		messages = self.env['mail.message'].search([('res_id',
+			'in', self.tasks.ids),('model','=','project.task')], order='write_date DESC')
+
+		for work in task_works:
+			self.event_lines += self.event_lines.create({
+				'task_id': work.task_id.id,
+				'name': work.name,
+				'unit_amount': work.unit_amount,
+				'date': work.write_date,
+				'user_id': work.user_id.id,
+				'project_id': self.id
+			})
+
+		for message in messages:
+			self.event_lines += self.event_lines.create({
+				'task_id': message.res_id,
+				'name': message.subtype_id.name,
+				'unit_amount': 0,
+				'date': message.write_date,
+				'user_id': message.create_uid.id,
+				'project_id': self.id
+			})
+		
 	# 5. Constraints and onchanges
 
     # 6. CRUD methods
@@ -45,3 +72,15 @@ class ProjectFeed(models.TransientModel):
 
     # 8. Business methods
     
+class ProjectEvent(models.TransientModel):
+
+	_name = 'project.event.line'
+
+	task_id = fields.Many2one('project.task', string='Task')
+	name = fields.Char(string='Event description')
+	unit_amount = fields.Float(string='Time used')
+	user_id = fields.Many2one('res.users',string='Done by')
+	project_id = fields.Many2one('project.project', string='Project')
+	date = fields.Datetime(string='Date')
+
+
