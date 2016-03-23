6ac5f4fa41ee6ee0e3898085ba45a73fd41e8639
Aleksi Savijoki <aleksi.savijoki@tawasta.fi>
2016-03-23 10:44:49 +0200

    Updated security rules.

--- __openerp__.py
+++ __openerp__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Project Customizations',
     'summary': 'Changes to Project app',
-    'version': '8.0.0.1.7',
+    'version': '8.0.0.1.8',
     'category': 'Project Management',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
