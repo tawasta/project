3c1d85dd5600b60f2cb653d858f73583547c3af0
Aleksi Savijoki <aleksi.savijoki@tawasta.fi>
2016-04-20 14:53:09 +0300

    Changed boolean values to be small characters.

--- project_customizations/__openerp__.py
+++ project_customizations/__openerp__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Project Customizations',
     'summary': 'Changes to Project app',
-    'version': '8.0.0.2.1',
+    'version': '8.0.0.2.2',
     'category': 'Project Management',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
