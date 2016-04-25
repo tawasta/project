757077339e805e52286dc317247e65d90ce34ce0
Aleksi Savijoki <aleksi.savijoki@tawasta.fi>
2016-04-25 12:23:45 +0300

    Increased version number.

--- project_event_feed/__openerp__.py
+++ project_event_feed/__openerp__.py
@@ -26,7 +26,7 @@
         Module for project events feed.
 
     """,
-    'version': '8.0.0.2.0',
+    'version': '8.0.0.2.1',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
     'license': 'AGPL-3',
