e14b47e279f7045036fc075adfd0eff126bea24d
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2016-09-01 11:13:35 +0300

    Remove a duplicat partner_id

--- project_extension/__openerp__.py
+++ project_extension/__openerp__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Project Extensions',
     'summary': 'Changes to Project app',
-    'version': '8.0.0.3.19',
+    'version': '8.0.0.3.20',
     'category': 'Project Management',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
