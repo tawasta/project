e589e0f22792e288fae7839110339d878a00075a
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2017-04-07 10:40:52 +0300

    Improve menu ordering

--- project_extension/__openerp__.py
+++ project_extension/__openerp__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Project Extensions',
     'summary': 'Changes to Project app',
-    'version': '8.0.0.3.22',
+    'version': '8.0.0.3.23',
     'category': 'Project Management',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
