c1e878af141d84c8a101b0688d703f3a7babe302
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2018-06-20 12:01:18 +0300

    Add new module for generating project sequences via button

---
+++ project_sequence_generator/models/project_project.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from odoo import models, api, fields
+
+
+class ProjectProject(models.Model):
+
+    _inherit = 'project.project'
+
+    code_generated = fields.Boolean(
+        string='Code generated',
+        help='Helper for sequence generator',
+        default=False,
+        readonly=True,
+    )
+
+    @api.multi
+    def action_sequence_to_name(self):
+        self.ensure_one()
+
+        IrSequence = self.env['ir.sequence']
+        sequence = IrSequence.next_by_code('project.project')
+        self.name = '%s/%s' % (sequence, self.name)
+        self.code_generated = True
+
+        return {
+            "type": "ir.actions.do_nothing",
+        }
