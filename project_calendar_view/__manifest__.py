dc554711b593229ab11fcaef06eaa0281ef0fb19
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2018-08-28 09:46:48 +0300

    Fix a field name

--- project_calendar_view/__manifest__.py
+++ project_calendar_view/__manifest__.py
@@ -23,7 +23,7 @@
     'name': 'Calendar view for projects',
     'summary': 'Calendar view for projects',
     'category': 'Project',
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.0.1',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
     'license': 'AGPL-3',
