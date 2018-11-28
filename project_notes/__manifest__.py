43c859930ab59413024382ceb5233ce09ad8e76a
difbit <timo.kekalainen@hotmail.com>
2018-11-28 09:41:29 +0200

    Version number changed

--- project_notes/__manifest__.py
+++ project_notes/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Project Notes',
     'summary': 'Adds notes for project and task ',
-    'version': '8.0.1.0.0',
+    'version': '10.0.1.0.0',
     'category': 'Project Management',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
