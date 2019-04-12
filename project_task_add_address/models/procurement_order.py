18bc1014be00298c017fc2cc8b22f14a420c44a3
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2019-04-12 09:05:18 +0300

    [REF] PEP8 autoformat

--- project_task_add_address/models/procurement_order.py
+++ project_task_add_address/models/procurement_order.py
@@ -5,7 +5,6 @@ from odoo import models
 
 
 class ProcurementOrder(models.Model):
-
     _inherit = 'procurement.order'
 
     def _create_service_task(self):
@@ -18,9 +17,10 @@ class ProcurementOrder(models.Model):
             if self.sale_line_id.order_id.description:
                 add_description = " / " + self.sale_line_id.order_id.description
 
-        res.name = '%s: %s / %s%s' % (self.origin or '', 
-                self.product_id.name,
-                self.sale_line_id.order_id.partner_shipping_id.name,
-                add_description)
+        res.name = '%s: %s / %s%s' % \
+                   (self.origin or '',
+                    self.product_id.name,
+                    self.sale_line_id.order_id.partner_shipping_id.name,
+                    add_description)
 
         return res
