84a5a9bc82a3aea37874502f55f197432dc69649
retropikzel <retropikzel@mailbox.org>
2020-05-19 17:45:36 +0300

    [MIG] New module

--- project_analytic_tags/__manifest__.py
+++ project_analytic_tags/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 ##############################################################################
 #
 #    Author: Oy Tawasta OS Technologies Ltd.
@@ -22,10 +21,10 @@
 {
     'name': 'Show analytic tags on projects',
     'summary': 'Show analytic tags on projects',
-    'version': '1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Project Management',
     'website': 'https://github.com/Tawasta/project',
-    'author': 'Oy Tawasta Technologies Ltd.',
+    'author': 'Tawasta',
     'license': 'AGPL-3',
     'application': False,
     'installable': True,
