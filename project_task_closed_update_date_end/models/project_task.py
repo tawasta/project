ce0fdbe2e9500ebac861e75525ffd25a9e16d73d
Jarmo Kortetjärvi <jarmo.kortetjarvi@gmail.com>
2020-12-04 14:31:15 +0200

    [ADD] New module project_task_closed_update_date_end
    Set project task end date when moving a task to closed stage

---
+++ project_task_closed_update_date_end/models/project_task.py
@@ -0,0 +1,27 @@
+from odoo import api
+from odoo import fields
+from odoo import models
+
+
+class ProjectTask(models.Model):
+    _inherit = "project.task"
+
+    @api.model
+    def create(self, vals):
+        if vals.get("stage_id"):
+            vals.update(self.update_date_end(vals["stage_id"]))
+
+            return super().create(vals)
+
+    @api.multi
+    def write(self, vals):
+        if vals.get("stage_id"):
+            vals.update(self.update_date_end(vals["stage_id"]))
+
+            return super().write(vals)
+
+    def update_date_end(self, stage_id):
+        project_task_type = self.env["project.task.type"].browse(stage_id)
+        if project_task_type.fold or project_task_type.closed:
+            return {"date_end": fields.Datetime.now()}
+        return {"date_end": False}
