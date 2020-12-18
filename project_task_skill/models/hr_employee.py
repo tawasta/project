0796a7cc6a5404fd10ad98e28f36f4354907a6df
Jarmo Kortetjärvi <jarmo.kortetjarvi@gmail.com>
2020-12-18 16:25:12 +0200

    [FIX] Field names

--- project_task_skill/models/hr_employee.py
+++ project_task_skill/models/hr_employee.py
@@ -1,17 +1,5 @@
-from odoo import api
-from odoo import fields
 from odoo import models
 
 
 class HrEmployee(models.Model):
     _inherit = "hr.employee"
-
-    skills_ids = fields.Many2many(
-        comodel_name="hr.skill", compute="_compute_skill_ids", string="Employee skills",
-    )
-
-    @api.onchange("skill_ids")
-    @api.depends("skill_ids")
-    def _compute_skill_ids(self):
-        for record in self:
-            record.skill_ids = record.skill_ids.mapped("skill_id")
