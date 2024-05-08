aa9db154ad5c1e69d885c2dcde48ac97a5013cde
odooProg <kalle.rantalainen01@gmail.com>
2024-05-08 14:51:10 +0300

    [ADD] added project resolution time and number of responses fields

---
+++ project_resolution_time/models/project_task.py
@@ -0,0 +1,32 @@
+from odoo import fields, models
+
+
+class ProjectTask(models.Model):
+
+    _inherit = "project.task"
+
+    resolution_time = fields.Float(string="Resolution time", compute="_compute_resolution_time")
+
+    number_of_responses = fields.Integer(string="Number of responses", compute="_compute_msg_count")
+
+    def _compute_resolution_time(self):
+        for record in self:
+            if record.date_assign and record.stage_id.name == "Done":
+                # Lasketaan aika taskin luomisen ja donen välillä eli luomisesta
+                # siihen hetkeen, kun tila muuttui doneksi
+                delta = record.date_last_stage_update - record.date_assign
+                delay_days = delta.days + delta.seconds / (24 * 3600)
+                record.resolution_time = delay_days
+            else:
+                record.resolution_time = False
+    
+    def _compute_msg_count(self):
+        for record in self:
+            count = 0
+            if record.website_message_ids:
+                for message in record.message_ids:
+                    if message.message_type == "comment":
+                        count += 1
+            record.number_of_responses = count
+
+                
