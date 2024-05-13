237ec9bd5643878002dba7207bfb5d995b124331
odooProg <kalle.rantalainen01@gmail.com>
2024-05-13 14:31:33 +0300

    [IMP] resolution time calculated from create_date and done date

--- project_resolution_time/models/project_task.py
+++ project_resolution_time/models/project_task.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class ProjectTask(models.Model):
@@ -6,19 +6,20 @@ class ProjectTask(models.Model):
     _inherit = "project.task"
 
     resolution_time = fields.Float(
-        string="Resolution time", compute="_compute_resolution_time", store=True
+        string="Resolution time", compute="_compute_resolution_time", store=True, default=None
     )
 
     number_of_responses = fields.Integer(
         string="Number of responses", compute="_compute_msg_count", store=True
     )
 
+    @api.depends('stage_id')
     def _compute_resolution_time(self):
+        # Tänne create date
         for record in self:
-            if record.date_assign and record.stage_id.name == "Done":
-                # Lasketaan aika taskin luomisen ja donen välillä eli luomisesta
-                # siihen hetkeen, kun tila muuttui doneksi
-                delta = record.date_last_stage_update - record.date_assign
+            if record.create_date and record.stage_id.name == "Done":
+                # Time between create date and resolvng date
+                delta = record.date_last_stage_update - self.create_date
                 delay_days = delta.days + delta.seconds / (24 * 3600)
                 record.resolution_time = delay_days
             else:
